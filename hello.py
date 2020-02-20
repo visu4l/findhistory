82fac1eb1882430e79e4c35650da4d56d58b235c
ChungHuiKim <visu4l@heykorean.com>
2020-02-20 10:51:35 +0900

    4th commit

--- hello.py
+++ hello.py
@@ -1,15 +1,19 @@
 
 
-def send_msg(msg):
+def print_msg(msg):
 	print("[+]", msg)
 
-alerts = ["[17-Feb-2020 22:22:46] WARNING: [pool www]", "[17-Feb-2020 21:15:57] NOTICE: [pool www]"]
+def change_code():
+	pass
+
+
+alerts = ["WARNING: [pool www]", "NOTICE: [pool www]"]
 
 for msg in alerts:
 
 	if msg.split(": ")[0].endswith("NOTICE"):
-	    send_msg("notice")
+	    pass
 	else:
-	    send_msg("not notice")
+	    print_msg("not notice")
 
-send_msg("done")
+print_msg("done")
