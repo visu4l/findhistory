df59bfd9a06e7deb93352728d7f8446b8c18995d
ChungHuiKim <visu4l@heykorean.com>
2020-02-20 10:43:46 +0900

    second commit

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 
+
+def send_msg(msg):
+	print("[+]", msg)
+
 alerts = ["[17-Feb-2020 22:22:46] WARNING: [pool www]", "[17-Feb-2020 21:15:57] NOTICE: [pool www]"]
 
 for msg in alerts:
 
 	if msg.split(": ")[0].endswith("NOTICE"):
-	    print("notice")
+	    send_msg("notice")
 	else:
-	    print("not notice")
-	    
+	    send_msg("not notice")
+
+send_msg("done")
